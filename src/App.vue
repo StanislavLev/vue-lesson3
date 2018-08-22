e88494ac6661cba0d527a88f2bc931126ef724ca
Stas <pschnk@gmail.com>
2018-08-22 11:44:26 -0700

    add SortSection

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,109 @@
 <template>
   <div id="app-root">
     <header><h1>{{title}}</h1></header>
-    <select v-model="sortOrder">
-      <option value="">    Original order                   </option>
-      <option value="AtoZ">Sort Stories by Teller's name A-Z</option>
-      <option value="ZtoA">Sort Stories by Teller's name Z-A</option>
-    </select>
-    <div class="puppy-name-grid">
-      <puppy v-for="(puppy, index) in sortedPuppiesArr" :name="puppy.name" :summary="puppy.summary" :imgLink="puppy.imgLink" :storyLink="puppy.storyLink" :key="index"></puppy>
+    <sortsection v-on:sortByName="sortPuppiesByName($event)"></sortsection>
+    <div id="puppy-card-grid">
+      <puppy v-for="(puppy, index) in puppies" :name="puppy.name" :summary="puppy.summary" :imgLink="puppy.imgLink" :storyLink="puppy.storyLink" :key="index"></puppy>
     </div>
   </div>
 </template>
 
 <script>
 
-import Puppy from './components/Puppy.vue'
+import Puppy          from './components/Puppy.vue';
+import SortSection from './components/SortSection.vue'
+
 
 export default {
   components: {
-    'puppy': Puppy
+    'puppy'       : Puppy,
+    'sortsection' : SortSection
   },
   data() {
     return {
       title: "Puppy Story Tellers!",
-      sortOrder: '',
       puppies: [
         { 
+          id: 1,
           name: 'Lucy',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory1',
           imgLink: 'http://www.placepuppy.net/1p/150/125'
         },
         { 
+          id: 2,
           name: 'Rocky',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory2',
           imgLink: 'http://www.placepuppy.net/2p/150/125'
         },
-        { 
+        {
+          id: 3,        
           name: 'Daisy',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory3',
           imgLink: 'http://www.placepuppy.net/3p/150/125'
-       },
+        },
         { 
+          id: 4,
           name: 'Buddy',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory4',
           imgLink: 'http://www.placepuppy.net/4p/150/125'
         },
         { 
+          id: 5,
           name: 'Lola',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory5',
           imgLink: 'http://www.placepuppy.net/5p/150/125'
         },
         { 
+          id: 6,
           name: 'Luna',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory6',
           imgLink: 'http://www.placepuppy.net/6p/150/125'
         },
         { 
+          id: 7,
           name: 'Bear',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory7',
           imgLink: 'http://www.placepuppy.net/7p/150/125'
         },
         { 
+          id: 8,
           name: 'Duke',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory8',
           imgLink: 'http://www.placepuppy.net/8p/150/125'
         },
         { 
+          id: 9,
           name: 'Sophie',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory9',
           imgLink: 'http://www.placepuppy.net/9p/150/125'
         },
         { 
+          id: 10,
           name: 'Tucker',
           summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip consequat...',
           storyLink: '#linkToStory10',
           imgLink: 'http://www.placepuppy.net/10p/150/125'
         }
-      ]
-    }
+      ],
+    };
   },
-  computed: {
-    sortedPuppiesArr(){
-      let notSortedArr = this.puppies.slice(0);
-      if(this.sortOrder === 'AtoZ') {
-        return notSortedArr.sort(function(a, b){return (a.name > b.name)?1:((a.name < b.name)?-1:0);});
-      }
-      else if(this.sortOrder === 'ZtoA') {
-        return notSortedArr.sort(function(a, b){return (a.name < b.name)?1:((a.name > b.name)?-1:0);});
+  methods: {
+    sortPuppiesByName(reversed){
+      console.log(reversed);
+      if(!reversed) {
+        return this.puppies.sort(function(a, b){return (a.name > b.name)?1:((a.name < b.name)?-1:0);});
       }
       else {
-        return this.puppies;
+        return this.puppies.sort(function(a, b){return (a.name < b.name)?1:((a.name > b.name)?-1:0);});
       }
     }
   }
---
+++ src/components/SortSection.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="sortSection">
+    <button @click="sortByName">
+      <h4>
+        Sort Stories by <br/> Teller's Name
+        <span v-show="sortedByName">
+          <span v-show="!reversed"> &#x25b2; </span>
+          <span v-show="reversed">  &#x25bc; </span>
+        </span>
+      </h4>
+    </button>
+    <button disabled>
+      <h4>Sort Stories by <br/> Rating </h4>
+    </button>
+    <button disabled>
+      <h4>Sort Stories by <br/> Date </h4>
+    </button>
+    <button disabled>
+      <h4>Sort Stories by <br/> Title </h4>
+    </button>
+  </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sortedByName: false,
+      reversed: false 
+    };
+  },
+  methods: {
+    sortByName(){
+      console.log(this.sortedByName);
+      console.log(this.reversed);
+      this.sortedByName?(this.reversed=!this.reversed):(this.sortedByName=true);
+      this.$emit('sortByName', this.reversed);
+    }
+  }
+}
+</script>
